50160edc3466aa578deea7926c224c1caf5436f8
Sitora Urazova-Lu <urazova.s@gmail.com>
2021-12-08 19:40:06 -0500

    Added styling

--- website/app.js
+++ website/app.js
@@ -1,5 +1,8 @@
+// Personal API Key for OpenWeatherMap API
+const apiKey = "<your_api_key>&units=imperial";
+
 /* Global Variables */
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
